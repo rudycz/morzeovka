078358295f7adc1e8f537b03aee02a9dc89b3336
rudycz <123189514+rudycz@users.noreply.github.com>
2023-03-09 10:43:27 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        basic.clearScreen()
+    } else if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.clearScreen()
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(30)
+basic.forever(function () {
+	
+})
